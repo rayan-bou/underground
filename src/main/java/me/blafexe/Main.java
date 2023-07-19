1dc0aef81f2f4a519a8384a16f52cc2e5766fe93
blafexe <blafexe@gmail.com>
2023-07-19 23:01:08 +0200

    Added EasyScoreboard

--- src/main/java/me/blafexe/Main.java
+++ src/main/java/me/blafexe/Main.java
@@ -3,6 +3,7 @@ package me.blafexe;
 import me.blafexe.job.JobEngine;
 import me.blafexe.loader.DummyZoneLoader;
 import me.blafexe.loader.ZoneLoader;
+import me.blafexe.scoreboard.TestScoreboardHandler;
 import me.blafexe.zone.DamageHandler;
 import me.blafexe.zone.ZoneHandler;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +26,8 @@ public class Main extends JavaPlugin {
 
         JobEngine jobEngine = new JobEngine();
         getServer().getPluginManager().registerEvents(jobEngine, this);
+
+        getServer().getPluginManager().registerEvents(new TestScoreboardHandler(this), this);
 
     }
 
